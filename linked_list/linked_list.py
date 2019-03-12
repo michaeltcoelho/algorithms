f5b06bb07795b701d1cccd45d808776c1618783c
michael coelho <michael.tcoelho@gmail.com>
2019-03-12 20:25:03 -0300

    Add python linked list impl.

---
+++ linked_list/linked_list.py
@@ -0,0 +1,85 @@
+from typing import Optional
+
+
+class Element:
+
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next: Optional[Element] = None
+    
+    def __str__(self):
+        return f'<Element(value={self.value})>'
+
+
+class LinkedList:
+
+    def __init__(self) -> None:
+        self.head = None
+
+    def push(self, new_element: Element) -> None:
+        if self.head is None:
+            self.head = new_element
+        else:
+            current_element = self.head
+            while current_element.next:
+                current_element = current_element.next
+            current_element.next = new_element
+
+    def get_position(self, position: int) -> Element:
+        if position == 0:
+            return self.head
+        current_position, current_element = 0, self.head
+        while current_element.next and current_position < position:
+            current_element = current_element.next
+            current_position += 1
+        return current_element
+
+    def insert(self, new_element: Element, position: int) -> None:
+        if position == 0:
+            new_element.next = self.head
+            self.head = new_element
+        else:
+            current_element = self.head
+            current_position = 0
+            previous_element = None
+            while current_element.next and current_position < position:
+                previous_element = current_element
+                current_element = current_element.next
+                current_position += 1
+            new_element.next = current_element
+            if previous_element:
+                previous_element.next = new_element
+
+    def show(self):
+        current_element = self.head
+        elements = []
+        while current_element.next:
+            elements.append(str(current_element))
+            current_element = current_element.next
+        elements.append(str(current_element))
+        print(elements)
+
+
+if __name__ == "__main__":
+    linked_list = LinkedList()
+    first_element = Element(1)
+    linked_list.push(first_element)
+    second_element = Element(2)
+    linked_list.push(second_element)
+    third_element = Element(3)
+    linked_list.push(third_element)
+
+    assert linked_list.head is first_element
+    assert linked_list.get_position(0) is first_element
+    assert linked_list.get_position(1) is second_element
+    assert linked_list.get_position(2) is third_element
+
+    fourth_element = Element(4)
+    linked_list.insert(fourth_element, 1)
+    assert linked_list.get_position(1) is fourth_element
+
+    fifth_element = Element(5)
+    linked_list.insert(fifth_element, 2)
+    assert linked_list.get_position(2) is fifth_element
+
+    linked_list.show()
